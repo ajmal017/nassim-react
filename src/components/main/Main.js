c879200c5931ae355ecb286d19205ac85c389425
elainechan <elaine.chan@outlook.com>
2018-09-10 23:16:24 -0400

    Adds Auth component modal, wrapping Register and Login tabs.;

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../home/Home';
 import Landing from '../landing/Landing';
+import Auth from '../auth/Auth'
 import Register from '../register/Register';
 import Login from '../login/Login';
 
@@ -11,6 +12,7 @@ export default class Main extends React.Component {
 			<Switch>
 				<Route path='/' exact component={Landing} />
 				<Route path='/home' component={Home} />
+				<Route path='/auth' component={Auth} />
 				<Route path='/register' component={Register} />
 				<Route path='/login' component={Login}/>
 			</Switch>
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,70 +1,64 @@
 import React from 'react';
-import { Button, Modal, FormGroup, ControlLabel, FormControl, HelpBlock, FieldGroup } from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 
 export default class Register extends React.Component {
 	constructor(props) {
 		super(props)
-		this.handleHide = this.handleHide.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-		this.getValidationState = this.getValidationState.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.getEmailValidationState = this.getEmailValidationState.bind(this);
+		this.getPasswordValidationState = this.getPasswordValidationState.bind(this);
 		this.state = {
 			show: false,
-			value: ''
+      email: '',
+      password: ''
 		}
 	}
-	handleHide() {
-		this.setState({
-			show: false
-		})
-	}
-	getValidationState() {
-    const length = this.state.value.length;
+
+  getEmailValidationState() {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(this.state.email).toLowerCase())) return 'success';
+    return 'error';
+  }
+	getPasswordValidationState() {
+    const length = this.state.password.length;
     if (length > 10) return 'success';
     else if (length > 5) return 'warning';
     else if (length > 0) return 'error';
     return null;
   }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
   }
 	render() {
 		return(
-			<div className="modal-container" style={{ height: 200 }}>
-        <Button
-          bsStyle="primary"
-          bsSize="large"
-          onClick={() => this.setState({ show: true })}
-        >
-          Launch contained modal
-        </Button>
-        <Modal
-          show={this.state.show}
-          onHide={this.handleHide}
-          container={this}
-          aria-labelledby="contained-modal-title"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title">
-              Register
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-					<form> 
-						<FormGroup controlId="formBasicText" validationState={this.getValidationState()} > 
-							<ControlLabel>Working example with validation</ControlLabel> 
-							<FormControl type="text" value={this.state.value} placeholder="Enter text" onChange={this.handleChange} /><FormControl.Feedback />
-							<HelpBlock>Validation is based on string length.</HelpBlock> 
-						</FormGroup> 
-					</form>
-
-
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleHide}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+      <form> 
+        <FormGroup controlId="formHorizontalEmail" validationState={this.getEmailValidationState()} > 
+          <ControlLabel>Email</ControlLabel> 
+          <FormControl
+            type="email"
+            value={this.state.email}
+            placeholder="Email"
+            onChange={this.handleEmailChange} 
+          />
+          <FormControl.Feedback />
+          <HelpBlock>Validation is based on format.</HelpBlock> 
+        </FormGroup> 
+        <FormGroup controlId="formHorizontalPassword" validationState={this.getPasswordValidationState()} > 
+          <ControlLabel>Password</ControlLabel> 
+          <FormControl
+            type="password"
+            value={this.state.password}
+            placeholder="Password"
+            onChange={this.handlePasswordChange} 
+          />
+          <FormControl.Feedback />
+          <HelpBlock>Validation is based on string length.</HelpBlock> 
+        </FormGroup>
+      </form>
 		)
 	}
 }
