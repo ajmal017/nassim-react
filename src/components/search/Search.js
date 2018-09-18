5246b040f66f9b3f3243c6835a85e760eef03188
elainechan <elaine.chan@outlook.com>
2018-09-17 22:13:00 -0400

    Makes search box with dropdown selection.

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,40 +1,38 @@
 import React from 'react';
 import axios from 'axios';
 import { FormGroup, FormControl } from 'react-bootstrap';
+import Autocomplete from 'react-autocomplete';
 
 export default class Search extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleSelect = this.handleSelect.bind(this);
 		this.state = {
-			searchTerm: ''
+			searchTerm: '',
+			stockData: [],
+			renderData: []
 		}
+		axios.get('https://api.iextrading.com/1.0/ref-data/symbols')
+				.then(response => {
+					this.setState({stockData: response.data});
+					console.log(this.stockData);
+				})
 	}
 
 	handleChange(e) {
 		e.preventDefault();
 		this.setState({searchTerm: e.target.value});
 		console.log(this.state.searchTerm);
-		const display = {
-			symbol: '',
-			name: ''
-		}
-		// call IEX symbols
-		axios.get('https://api.iextrading.com/1.0/ref-data/symbols')
-				.then(response => {
-					var selections = response.data.filter(entry => {
-						entry.name === this.state.searchTerm || entry.symbol === this.state.searchTerm;
-					})
-					console.log(selections);
-						// TODO: check if match exists in symbols or names in response.data
-						// TODO: populate dropdown with clickable search results
-				})
+		let selections = this.state.stockData.filter(entry => {
+			entry.name === this.state.searchTerm || entry.symbol === this.state.searchTerm;
+		});
+		console.log(`selections`, selections);
 	}
 	populateSelection() {
 
 	}
-	handleSubmit() {
+	handleSelect() {
 		// TODO:
 		// needs symbol value of selected entry
 		// pass on submitted symbol value to Stock component
@@ -45,7 +43,23 @@ export default class Search extends React.Component {
 	render() {
 		return (
 			<div>
-				<form onSubmit={this.handleSubmit}>
+				<Autocomplete
+					getItemValue={(item) => item.symbol}
+					items={this.state.stockData}
+					renderItem={(item, isHighlighted) =>
+						<div style={{ background: isHighlighted ? 'lightgray' : 'white' }} key={item.symbol}>
+							{item.symbol} - {item.name}
+						</div>
+					}
+					value={this.state.searchTerm}
+					onChange={this.handleChange}
+					onSelect={this.handleSelect}
+				/>
+			</div>
+		)
+	}
+}
+/*
 					<FormGroup
 						controlId="formBasicText"
 					>
@@ -56,8 +70,12 @@ export default class Search extends React.Component {
 							onChange={this.handleChange}
 						/>
 					</FormGroup>
-				</form>
-			</div>
-		)
-	}
-}
+/*
+					var selections = response.data.filter(entry => {
+						entry.name === this.state.searchTerm || entry.symbol === this.state.searchTerm;
+					})
+					console.log(selections);
+					
+						// TODO: check if match exists in symbols or names in response.data
+						// TODO: populate dropdown with clickable search results
+*/
